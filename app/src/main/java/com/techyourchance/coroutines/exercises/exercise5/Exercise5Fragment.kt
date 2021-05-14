2ab789a3746d6134f7b47a2a39bf36bbe1e91a43
kangraemin <remin1994@gmail.com>
2021-05-14 14:47:51 +0900

    Encapsulate method using background thread in presentation layer

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise5/Exercise5Fragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise5/Exercise5Fragment.kt
@@ -57,7 +57,8 @@ class Exercise5Fragment : BaseFragment() {
             logThreadInfo("button callback")
             job = coroutineScope.launch {
                 btnGetReputation.isEnabled = false
-                val reputation = getReputationForUser(edtUserId.text.toString())
+                logThreadInfo("getReputationForUser()")
+                val reputation = getReputationEndpoint.getReputation(edtUserId.text.toString())
                 Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT).show()
                 btnGetReputation.isEnabled = true
             }
@@ -70,13 +71,6 @@ class Exercise5Fragment : BaseFragment() {
         super.onStop()
         job?.cancel()
         btnGetReputation.isEnabled = true
-    }
-
-    private suspend fun getReputationForUser(userId: String): Int {
-        return withContext(Dispatchers.Default) {
-            logThreadInfo("getReputationForUser()")
-            getReputationEndpoint.getReputation(userId)
-        }
     }
 
     private fun logThreadInfo(message: String) {
